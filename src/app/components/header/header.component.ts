950b67c2ea34423805c30027c00c1e013485c1af
sergey-dev <serbro7@gmail.com>
2022-04-18 13:15:10 +0200

    Logout button upper right
    Enabled logout button on the right side of the header if a user is logged in

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthStateService } from 'src/app/shared/auth-state.service';
+import { TokenService } from 'src/app/shared/token.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isSignedIn!: boolean;
+  constructor(
+    private auth: AuthStateService,
+    public router: Router,
+    public token: TokenService
+  ) { }
 
   ngOnInit(): void {
+    this.auth.userAuthState.subscribe((val) => {
+      this.isSignedIn = val;
+    });
+  }
+
+  //Signout
+  signOut() {
+    this.auth.setAuthState(false);
+    this.token.removeToken();
+    this.router.navigate(['login']);
   }
 
 }
