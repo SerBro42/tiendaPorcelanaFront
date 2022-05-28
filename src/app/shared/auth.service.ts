0499af0754eb1f88e2db7bfca772e20644900633
sergey-dev <serbro7@gmail.com>
2022-05-28 12:05:54 +0200

    Environment api URL
    Updated API URL to make use of the environment variable, instead of declaring it each time in each service

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 // User interface
 export class User {
@@ -20,14 +21,14 @@ export class AuthService {
 
     // User registration
     register(user: User): Observable<any> {
-      return this.http.post('http://127.0.0.1:8000/api/auth/register', user);
+      return this.http.post(environment.apiUrl+'/api/auth/register', user);
     }
     // Login
     signin(user: User): Observable<any> {
-      return this.http.post<any>('http://127.0.0.1:8000/api/auth/login', user);
+      return this.http.post<any>(environment.apiUrl+'/api/auth/login', user);
     }
     // Access user profile
     profileUser(): Observable<any> {
-      return this.http.get('http://127.0.0.1:8000/api/auth/user-profile');
+      return this.http.get(environment.apiUrl+'/api/auth/user-profile');
     }
 }
--- src/app/shared/contact-form.service.ts
+++ src/app/shared/contact-form.service.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactFormService {
 
-  private url: string = "http://localhost:8000/api";
   constructor(private http: HttpClient) { }
   insertarFormulario(form:any): Observable<any>{
-    return this.http.post(this.url+'/contactForm/add',form);
+    return this.http.post(environment.apiUrl+'/api/contactForm/add',form);
   }
 
 }
